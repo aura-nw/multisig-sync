50da3cb1ed51c8079fc53e344b1ce73fb602474a
Phan Anh Tuan <fibonacci998@gmail.com>
2022-01-20 10:23:37 +0700

    init job every 5 second get new address

--- src/repositories/isafe.repository.ts
+++ src/repositories/isafe.repository.ts
@@ -2,4 +2,7 @@ import { IBaseRepository } from './ibase.repository';
 
 export interface ISafeRepository extends IBaseRepository {
     checkExistsSafeAddress(listAddress: string[]);
+
+    findSafeNotInListAddress(listAddress: string[]);
+    
 }
