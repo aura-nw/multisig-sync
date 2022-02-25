c9b40518b07911c8aef73707f2b9dfd50f9efb02
Tuan Phan Anh <fibonacci998@gmail.com>
2022-02-25 14:16:05 +0700

    create cicd for staging

--- ci/updateManifest.sh
+++ ci/updateManifest.sh
@@ -16,6 +16,10 @@ elif [ ${GITHUB_REF_NAME} = "dev" ]
 then
   echo 'This is dev branch'
   cd ${REPO_MANIFEST_ENV_DEV}
+elif [ ${GITHUB_REF_NAME} = "staging" ]
+then
+  echo 'This is staging branch'
+  cd ${REPO_MANIFEST_ENV_STAGING}
 else
   exit
 fi
