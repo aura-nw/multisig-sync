8812dae868049ccd3a04f9f02670c7f837badde8
doquockhanhan <doquockhanhan@gmail.com>
2022-03-25 17:26:43 +0700

    create job to update failed tx that have txhash

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,10 +15,12 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { SyncRestService } from './services/impls/sync-rest.service';
 import { HttpModule } from '@nestjs/axios';
 import { AppController } from './controllers/websocket.controller';
+import { MultisigTransactionRepository } from './repositories/impls/multisig-transaction.repository';
 const entities = [
     ENTITIES_CONFIG.AURA_TX,
     ENTITIES_CONFIG.SAFE,
     ENTITIES_CONFIG.CHAIN,
+    ENTITIES_CONFIG.MULTISIG_TRANSACTION,
 ];
 const controllers = [AppController];
 // @Global()
@@ -57,6 +59,10 @@ const controllers = [AppController];
             provide: REPOSITORY_INTERFACE.ICHAIN_REPOSITORY,
             useClass: ChainRepository,
         },
+        {
+            provide: REPOSITORY_INTERFACE.IMULTISIG_TRANSACTION_REPOSITORY,
+            useClass: MultisigTransactionRepository,
+        }
     ],
 })
 export class AppModule {}
