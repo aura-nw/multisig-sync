a491cde7525033e39098385136dff06959370dab
harisato <pp.quochai@gmail.com>
2023-06-15 10:50:45 +0700

    fix (gh-actions): config git email

--- ci/updateManifest.sh
+++ ci/updateManifest.sh
@@ -42,6 +42,7 @@ curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack
 rm kustomize
 
 git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "admin@aura.network"
 git add . 
 git commit -m "Update image to ${CONTAINER_RELEASE_IMAGE}"
 git push
